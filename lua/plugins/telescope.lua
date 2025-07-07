b56e8fc83445adbf47422fbeb642687dbb546a7e
Themos Papatheofanous <themos360@gmail.com>
2025-07-07 17:40:48 +0300

    add tree-sitter and theme

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,9 +2,29 @@ return {
 
 	{
 		'nvim-telescope/telescope.nvim',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+		},
 		config = function()
+			require('telescope').setup {
+				pickers = {
+					find_files = {
+						theme = "ivy"
+					}
+				},
+				extensions = {
+					fzf = {}
+				}
+			}
+
+			require("telescope").load_extension("fzf")
+			vim.keymap.set('n', '<space>ff', require('telescope.builtin').find_files)
+			vim.keymap.set('n', '<space>fh', require('telescope.builtin').help_tags)
+			vim.keymap.set('n', '<space>fs', function() 
+				require('telescope.builtin').grep_string({ search = vim.fn.input(" Grep > ") });
+			end)
+
 		end
           },
-          -- Other plugins
 }
